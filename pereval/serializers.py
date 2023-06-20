ab99807bb5b82d954e2883ca819063766732eb6b
Ravil.Flyday <rmm68@mail.ru>
2023-06-20 12:07:55 +0300

    Add ImageSerializer (raw data only)

--- pereval/serializers.py
+++ pereval/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
-from .models import PerevalAdded, Coords
+from .models import PerevalAdded, Coords, Images
 
+
+class ImagesSerializer(serializers.ModelSerializer):
+    img = serializers.URLField()
+
+    class Meta:
+        model=Images
+        fields = ['title', 'img']
 class CoordsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Coords
@@ -10,15 +17,23 @@ class CoordsSerializer(serializers.ModelSerializer):
 
 class PerevalAddedSerializer(serializers.ModelSerializer):
     coords = CoordsSerializer()
-
+    images = ImagesSerializer(many=True)
 
     class Meta:
         model = PerevalAdded
-        fields = '__all__'
+        fields = ('id', 'beauty_title', 'title', 'other_titles', 'connect', 'coords', 'images', 'status')
+
 
     def create(self, validated_data):
-        coord_data = validated_data.pop('coords')
-        coord_ = Coords.objects.create(**coord_data)
+        coord_data = validated_data.pop('coords') #извлекаем из запроса координаты
+        coord_ = Coords.objects.create(**coord_data) #и сохраняем в базе
+        images = validated_data.pop('images')
 
         pereval_added = PerevalAdded.objects.create(coords=coord_, **validated_data)
+
+        for image in images: #извлекаем ссылки на картинки и сохраняем в бд
+            img_ = image.pop('img')
+            title_ = image.pop('title')
+            Images.objects.create(pereval=pereval_added, img=img_, title=title_) #создали перевал
+
         return pereval_added
