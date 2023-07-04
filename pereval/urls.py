03bfd9dbc7de181bf814e609e52d344aca1b70ea
Ravil.Flyday <rmm68@mail.ru>
2023-07-04 09:00:49 +0300

    Add Swagger page

--- pereval/urls.py
+++ pereval/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from .views import SubmitData, UpdateSubmitData, UserPerevalList
 
 urlpatterns = [
     path('submitdata/', SubmitData.as_view()),
     path('submitdata/<int:pk>/', UpdateSubmitData.as_view()),
     path('submitdata/user__email=<str:email>/', UserPerevalList.as_view()),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
 ]
